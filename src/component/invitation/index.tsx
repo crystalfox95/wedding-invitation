ae7f29a15428a74052362fb0079318b8364e6842
crystalfox95 <hunter95001kong@gmail.com>
2026-02-21 06:05:46 +0900

    Update index.tsx

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -24,18 +24,16 @@ export const Invitation = () => {
       <h2 className="english">Invitation</h2>
 
       <div className="break" />
-
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
+      <div className="content">따스한 봄날</div>
       <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
+      <div className="content">저희 두 사람이 사랑의 약속을 하려 합니다.</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
+      <div className="content">햇살이 머무는 날처럼</div>
+      <div className="content">서로에게 늘 따뜻한 사람이 되어</div>
+      <div className="content">함께 걸어가겠습니다.</div>
       <div className="break" />
       <div className="content">기쁜날 함께 하시어</div>
       <div className="content">저희의 앞날을 축복해 주세요.</div>
-
       <div className="break" />
 
       <div className="name">
